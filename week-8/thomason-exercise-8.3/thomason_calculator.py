7f2eb42bffdb0aa603dadf5e3776b3997b5e0d75
Tyson Thomason <tthomason@mailserviceslc.com>
2019-06-19 15:26:45 -0500

    finished 8.3

---
+++ week-8/thomason-exercise-8.3/thomason_calculator.py
@@ -0,0 +1,16 @@
+def add(param1, param2):
+    return param1 + param2
+
+
+
+def subtract(param1, param2):
+    return param1 - param2
+
+
+
+def divide(param1, param2):
+    return param1 + param2
+    
+print(add(1, 3))
+print(subtract(5, 3))
+print(divide(20, 5))
