8c147ac2ef914dcab5d55d92375d400c6508cd04
Tyson Thomason <tthomason@mailserviceslc.com>
2019-06-27 15:21:38 -0500

    finished 9.2

---
+++ week-9/thomason-exercise-9.2/thomason-user-service.py
@@ -0,0 +1,39 @@
+#/*
+#============================================
+#; Title: User Service
+#; File Name: thomason-user-service.py
+#; Author: William Thomason
+#; Date:   27 June 2019
+#; Description: Insert User
+#;===========================================
+#*/
+
+from pymongo import MongoClient
+
+import pprint
+
+import datetime
+
+client = MongoClient('localhost', 27017)
+
+db = client.web335
+
+user = {
+
+    "first_name": "Jesse",
+
+    "last_name": "Freeman",
+
+    "email": "freeman@me.com",
+
+    "employee_id": "6825491",
+
+    "date_created": datetime.datetime.utcnow()
+
+}
+
+user_id = db.users.insert_one(user).inserted_id
+
+print(user_id)
+
+pprint.pprint(db.users.find_one())
